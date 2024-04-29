b8faf3c2d09f73232bb0f6bd18a6759191c3f091
Amronos <134804732+Amronos@users.noreply.github.com>
2024-04-29 18:56:15 +0530

    switch tags to datagen

---
+++ src/main/java/net/amronos/createaddoncompatibility/Tags/ForgeTags.java
@@ -0,0 +1,23 @@
+package net.amronos.createaddoncompatibility.Tags;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.material.Fluid;
+
+public class ForgeTags {
+    public static class Fluids {
+        public static final TagKey<Fluid> CRUDE_OIL = tag("crude_oil");
+        public static final TagKey<Fluid> DIESEL = tag("diesel");
+        public static final TagKey<Fluid> ETHANOL = tag("ethanol");
+        public static final TagKey<Fluid> GASOLINE = tag("gasoline");
+        public static final TagKey<Fluid> KEROSENE = tag("kerosene");
+        public static final TagKey<Fluid> LIQUID_PLASTIC = tag("liquid_plastic");
+        public static final TagKey<Fluid> LUBRICANT = tag("lubricant");
+        public static final TagKey<Fluid> PLANTOIL = tag("plantoil");
+
+        private static TagKey<Fluid> tag(String name) {
+            return FluidTags.create(new ResourceLocation("forge", name));
+        }
+    }
+}
---
+++ src/main/java/net/amronos/createaddoncompatibility/datagen/ForgeFluidTagGenerator.java
@@ -0,0 +1,51 @@
+package net.amronos.createaddoncompatibility.datagen;
+
+import net.amronos.createaddoncompatibility.CreateAddonCompatibility;
+import net.amronos.createaddoncompatibility.Tags.ForgeTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.FluidTagsProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import java.util.concurrent.CompletableFuture;
+
+
+public class ForgeFluidTagGenerator extends FluidTagsProvider {
+    public ForgeFluidTagGenerator(PackOutput p_255941_, CompletableFuture<HolderLookup.Provider> p_256600_, ExistingFileHelper existingFileHelper) {
+        super(p_255941_, p_256600_, CreateAddonCompatibility.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ForgeTags.Fluids.CRUDE_OIL)
+                .addOptional(new ResourceLocation("ad_astra", "oil"))
+                .addOptional(new ResourceLocation("ad_astra", "flowing_oil"))
+                .addOptional(new ResourceLocation("pneumaticcraft", "oil"))
+                .addOptional(new ResourceLocation("pneumaticcraft", "flowing_oil"))
+                .addOptional(new ResourceLocation("tfmg", "crude_oil_fluid"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_crude_oil_fluid"));
+        this.tag(ForgeTags.Fluids.DIESEL)
+                .addOptional(new ResourceLocation("tfmg", "diesel"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_diesel"));
+        this.tag(ForgeTags.Fluids.ETHANOL)
+                .addOptional(new ResourceLocation("createaddition", "bioethanol"))
+                .addOptional(new ResourceLocation("createaddition", "flowing_bioethanol"));
+        this.tag(ForgeTags.Fluids.GASOLINE)
+                .addOptional(new ResourceLocation("tfmg", "gasoline"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_gasoline"));
+        this.tag(ForgeTags.Fluids.KEROSENE)
+                .addOptional(new ResourceLocation("tfmg", "kerosene"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_kerosene"));
+        this.tag(ForgeTags.Fluids.LIQUID_PLASTIC)
+                .addOptional(new ResourceLocation("pneumaticcraft", "plastic"))
+                .addOptional(new ResourceLocation("pneumaticcraft", "flowing_plastic"))
+                .addOptional(new ResourceLocation("tfmg", "liquid_plastic"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_liquid_plastic"));
+        this.tag(ForgeTags.Fluids.LUBRICANT)
+                .addOptional(new ResourceLocation("tfmg", "lubrication_oil"))
+                .addOptional(new ResourceLocation("tfmg", "flowing_lubrication_oil"));
+        this.tag(ForgeTags.Fluids.PLANTOIL)
+                .addOptional(new ResourceLocation("garnished", "peanut_oil"));
+    }
+}
